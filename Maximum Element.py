78515653088eea9621430b862bdd57573bf23ac5
Puja Roy <62675121+pujaroy280@users.noreply.github.com>
2021-10-03 10:42:15 -0400

    Add files via upload

---
+++ Maximum Element.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getMax' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts STRING_ARRAY operations as parameter.
+#
+
+
+if __name__ == "__main__":
+    stack = []
+    ops_cnt = int(input().strip())
+    max_elem = 0
+    
+    for _ in range(ops_cnt):
+        args = list(map(int, input().strip().split()))
+        
+        if args[0] == 1:
+            max_elem = max(max_elem, args[1])
+            stack.append(args[1])
+        if args[0] == 2:
+            if stack.pop() == max_elem:
+                if len(stack) > 0:
+                    max_elem = max(stack)
+                else:
+                    max_elem = 0
+        if args[0] == 3:
+            print(max_elem)
